3280b660a72d76664f67447d92169dc46783c185
Jules FUSELIER <jules.fuselier@etu.univ-amu.fr>
2026-01-22 17:33:25 +0100

    ft : ajout d'une ligne de tableau dynamique pour afficher la valeur courante et son unité, remplacement du paragraphe par une ligne de tableau et adaptation de la coloration

--- index.js
+++ index.js
@@ -35,33 +35,41 @@ const O_para1 = document.getElementById("paragraph-1");
 const O_section = document.getElementById("section-1");
 const O_para2 = O_section.appendChild(document.createElement("paragraph-2"));
 const O_table = document.getElementById("table-1");
-
+const O_row = document.getElementById("row-1");
 I_compteur = 0;
-
+const B_IS_TEMPERATURE = true
 
 
 function changeParagraph() {
-    O_para1.textContent = "";
+    // O_para1.textContent = "";
+    O_row.textContent = "";
     O_para2.textContent = "";
     const I_value = A_randomArray[I_compteur];
-    O_para1.textContent += I_value + " " + A_data["type-of-temperature"];
+
+    const O_NewCell = O_row.insertCell(0);
+    O_NewCell.textContent = I_value;
+    if (B_IS_TEMPERATURE ) {
+        const O_NewCell2 = O_row.insertCell(1);
+        O_NewCell2.textContent = A_data["type-of-temperature"];
+    }
+
 
     const O_NewRow = O_table.insertRow()
     O_NewRow.insertCell(0).textContent = I_compteur;
     O_NewRow.insertCell(1).textContent = I_value;
 
     if ( I_value <= 0 ) {
-        O_para1.className = A_data["cold-color"];
+        O_row.className = A_data["cold-color"];
         O_para2.textContent += A_data["cold-message"];
     }
     else if ( 1 <= I_value && I_value <= 20 ) {
-        O_para1.className = A_data["mild-color"];
+        O_row.className = A_data["mild-color"];
     }
     else if ( 21 <= I_value && I_value <= 30 ) {
-        O_para1.className = A_data["warm-color"];
+        O_row.className = A_data["warm-color"];
     }
     else {
-        O_para1.className = A_data["hot-color"];
+        O_row.className = A_data["hot-color"];
         O_para2.textContent += A_data["hot-message"];
 
     }
